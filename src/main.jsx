79b854365c223c957fc4711c0750260f9b00b7cc
SoloGudLyf <solomonteiko0@gmail.com>
2025-11-26 06:16:55 +0000

    feat: Bundle files

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode} from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { RenderImages } from "./components/renderImages";
-import {Header} from "./components/header"
+
 import HeaderImageLink from "./components/score-image";
 
-
-
-createRoot(document.getElementById("root")).render(<StrictMode>
-  <HeaderImageLink/>
-</StrictMode>);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <HeaderImageLink />
+  </StrictMode>
+);
